aa6dec330ac214670efac4a173761752046abc6c
Anwar Rahim <72375327+anwarrahim@users.noreply.github.com>
2023-04-14 05:31:06 +0000

    make the print statement

--- classes.py
+++ classes.py
@@ -4,10 +4,18 @@ class person():
         self.age = age
         self.work= work
     def introduction(self):
+        print("my name is " + self.name + " and i am " + str(self.age) + " years old and i am a " + self.work)
 
     def accupation(self):
-    
-    
+        print("my name is " + self.name + " and i am a " + self.work)
+
+#make the objects
+
+person1 = person("Anwar Rahim", 20 , "engineer")
+person2 = person("Ahmed", 20 , "doctor")
+person3 = person("farhan", 20 , "programmer")
+
+
 
 
 
