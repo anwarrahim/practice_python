202b207ccc9285efd8addddec5cd068235bf1d49
Anwar Rahim <72375327+anwarrahim@users.noreply.github.com>
2023-04-14 05:26:10 +0000

    add new content and make the methods

--- classes.py
+++ classes.py
@@ -1,6 +1,14 @@
 class person():
-    def __init__(self, name, age):
+    def __init__(self, name, age, work):
         self.name = name
         self.age = age
-        
+        self.work= work
+    def introduction(self):
+
+    def accupation(self):
+    
+    
+
+
+
         
