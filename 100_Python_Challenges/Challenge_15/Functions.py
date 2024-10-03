22a65ae3c12910beba03d017eae3a961404d7d10
Anwar Rahim <anwargithub@gmail.com>
2024-10-03 22:13:18 +0500

    functions upadated

--- 100_Python_Challenges/Challenge_15/Functions.py
+++ 100_Python_Challenges/Challenge_15/Functions.py
@@ -6,3 +6,8 @@ def square(num):
     return num ** 2
 area = square(5)
 print(area)
+
+"""
+Task 2:
+Write a function is_prime that takes a number as input and returns True if it's prime, and False otherwise.
+"""
