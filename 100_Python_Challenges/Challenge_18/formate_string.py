5c38b46f844b35449e60ff420430b853bda6e714
Anwar Rahim <anwargithub@gmail.com>
2024-10-06 11:13:35 +0500

    add new challenge

--- 100_Python_Challenges/Challenge_18/formate_string.py
+++ 100_Python_Challenges/Challenge_18/formate_string.py
@@ -3,3 +3,7 @@ age = 3
 print("Hi {}, How are you, I know you are {} old. right?".format(name, age))
 
 
+depart = 'Software Eng'
+experience = 2
+print(f"I am {depart} and my experience is {experience} years".format(experience,depart))
+
