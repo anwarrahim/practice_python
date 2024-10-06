99ab14e3b8278f29e6e7514d53f0dc3078facfa2
Anwar Rahim <anwargithub@gmail.com>
2024-10-06 11:23:08 +0500

    add new challenge

--- 100_Python_Challenges/Challenge_18/formate_string.py
+++ 100_Python_Challenges/Challenge_18/formate_string.py
@@ -1,3 +1,5 @@
+from dask.array import average
+
 name = "Amal"
 age = 3
 print("Hi {}, How are you, I know you are {} old. right?".format(name, age))
@@ -5,5 +7,10 @@ print("Hi {}, How are you, I know you are {} old. right?".format(name, age))
 
 depart = 'Software Eng'
 experience = 2
-print(f"I am {depart} and my experience is {experience} years".format(experience,depart))
+print("I am {depart} and my experience is {experience} years".format(experience=experience,depart=depart))
 
+
+# decimal place
+
+house_area = 23.343
+print("My house are is {house_area:.2f} and its not much large. But I like it".format(house_area=house_area))
